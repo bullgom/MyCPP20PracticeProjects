96d541c6e47dbd60aa78660f45abfabb699f7d12
bullgom <bullgom0213@gmail.com>
2021-02-28 20:59:58 +0900

    출력 담당 클래스

---
+++ Authentication/view.cpp
@@ -0,0 +1,6 @@
+#include "view.h"
+
+void View::displayMainMenu()
+{
+    
+}
---
+++ Authentication/view.h
@@ -0,0 +1,11 @@
+#include <map>
+
+class View
+{
+private:
+    std::map<char, 
+        public : View();
+    void displayMainMenu();
+    void displayError();
+    void displayWarning();
+};
