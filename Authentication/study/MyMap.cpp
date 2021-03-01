b60580b409ca6373dca4621b9e483dfc374baf0d
bullgom <bullgom0213@gmail.com>
2021-03-01 11:02:38 +0900

    ordered와 unordered 비교

--- Authentication/study/MyMap.cpp
+++ Authentication/study/MyMap.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <unordered_map>
 #include <iostream>
 #include <string>
 
@@ -17,15 +18,38 @@
  * std::map                  1.81    1.629   0.988       1.300
  * std::unordered_map        0.534   0.294   0.087       0.087 <- 훨씬 빠름
  */
+
+template <class Container>
+void printMap(const Container &container)
+{
+    for (const auto &[name, age] : container)
+    {
+        std::cout << name << " is " << age << " years old" << std::endl;
+    }
+}
+
 int main()
 {
-    
+
     std::map<std::string, int> ages{{"James", 10}, {"Inyoung", 20}, {"Kim", 30}};
+    std::cout << "Ordered" << std::endl;
+    printMap(ages);
+    std::cout << std::endl;
 
-    for(const auto& [name, age] : ages)
-        std::cout << name << " is " << age << " years old" << std::endl;
-    
+    ages["Bullgom"] = 40;
+    std::cout << "Modified ordered" << std::endl;
+    printMap(ages);
+    std::cout << std::endl;
 
+    std::unordered_map<std::string, int> uo_ages{{"James", 10}, {"Inyoung", 20}, {"Kim", 30}};
+
+    std::cout << "Unordered" << std::endl;
+    printMap(uo_ages);
+    std::cout << std::endl;
+
+    std::cout << "Modified un ordered" << std::endl;
+    uo_ages["Bullgom"] = 40;
+    printMap(uo_ages);
 
     return 0;
 }
